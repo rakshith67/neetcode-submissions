82db6d3e189f2d0357beb5f6d85c23a68126d4b5
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-06-20 13:55:54 +0530

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.java
@@ -0,0 +1,81 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        //mergeSort(nums, 0, nums.length - 1);
+        quickSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private static void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int partition = partition(nums, left, right);
+            quickSort(nums, left, partition - 1);
+            quickSort(nums, partition + 1, right);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    private static void mergeSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, left, mid);
+            mergeSort(nums, mid + 1, right);
+            merge(nums, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] nums, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = nums[left + i];
+        }
+        for (int i = 0; i < n2; i++) {
+            rightArray[i] = nums[mid + 1 + i];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] < rightArray[j]) {
+                nums[k++] = leftArray[i++];
+            } else {
+                nums[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] < rightArray[j]) {
+                nums[k++] = leftArray[i++];
+            } else {
+                nums[k++] = rightArray[j++];
+            }
+        }
+
+        while (i < n1) {
+            nums[k++] = leftArray[i++];
+        }
+
+        while (j < n2) {
+            nums[k++] = rightArray[j++];
+        }
+    }
+}
