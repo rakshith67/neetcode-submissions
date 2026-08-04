86e0ff8130201ae10024851af55e122cf428a048
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-08-04 22:47:18 +0530

    Add: partition-to-k-equal-sum-subsets - submission-2

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum % k != 0) {
+            return false;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length / 2; i++) {
+            int tmp = nums[i];
+            nums[i] = nums[nums.length - i - 1];
+            nums[nums.length - i - 1] = tmp;
+        }
+        boolean[] used = new boolean[nums.length];
+        return canPartition(nums, 0, k, 0, sum / k, used);
+    }
+
+    private boolean canPartition(int[] nums, int index, int k, int sum, int target, boolean[] used) {
+        if (k == 0) {
+            return true;
+        }
+        if (sum == target) {
+            return canPartition(nums, 0, k - 1, 0, target, used);
+        }
+        for (int i = index; i < nums.length; i++) {
+            if (used[i] || sum + nums[i] > target) {
+                continue;
+            }
+            used[i] = true;
+            if (canPartition(nums, i + 1, k, sum + nums[i], target, used)) {
+                return true;
+            }
+            used[i] = false;
+        }
+        return false;
+    }
+}
