a7865300f03eaa451f05ac5ca095e06a90de1489
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-08-12 15:56:26 +0530

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int[] parent = new int[edges.length + 1];
+        int[] rank = new int[edges.length + 1];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+
+        for (int[] edge: edges) {
+            if (!union(parent, rank, edge[0], edge[1])) {
+                return edge;
+            }
+        }
+        return new int[0];
+    }
+
+    private int find(int[] parent, int u) {
+        int p = parent[u];
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    private boolean union(int[] parent, int[] rank, int u, int v) {
+        int parent1 = find(parent, u);
+        int parent2 = find(parent, v);
+
+        if (parent1 == parent2) {
+            return false;
+        }
+
+        if (rank[parent1] >= rank[parent2]) {
+            parent[parent2] = parent1;
+            rank[parent1] += rank[parent2];
+        } else {
+            parent[parent1] = parent2;
+            rank[parent2] += rank[parent1];
+        }
+        return true;
+    }
+}
