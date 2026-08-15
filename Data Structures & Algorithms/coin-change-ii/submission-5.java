035a29b34b24c67952500873be4be8264b1a7e34
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-08-15 14:55:16 +0530

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int change(int amount, int[] coins) {
+        int length = coins.length;
+        Arrays.sort(coins);
+        int[][] result = new int[length + 1][amount + 1];
+
+        for (int i = 0; i <= length; i++) {
+            result[i][0] = 1;
+        }
+
+        for (int i = length - 1; i >= 0; i--) {
+            for (int a = 1; a <= amount; a++) {
+                if (a >= coins[i]) {
+                    result[i][a] = result[i + 1][a];
+                    result[i][a] += result[i][a - coins[i]];
+                }
+            }
+        }
+        return result[0][amount];
+    }
+}
