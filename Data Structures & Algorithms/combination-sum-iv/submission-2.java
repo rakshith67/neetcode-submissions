f62dfa61047c5f711bff321c091cbf17c034f3f6
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-09-01 17:41:44 +0530

    Add: combination-sum-iv - submission-2

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] result = new int[target + 1];
+        result[0] = 1;
+        for (int i = 0; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i - nums[j] >= 0) {
+                    result[i] += result[i - nums[j]];
+                }
+            }
+        }
+        return result[target];
+    }
+}
