aaefa2d7ff24c1272830f93ef3c9e313607aed7d
rakshith67 <38179559+rakshith67@users.noreply.github.com>
2026-09-01 11:34:25 +0530

    Add: permutations-ii - submission-2

---
+++ Data Structures & Algorithms/permutations-ii/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        result = new ArrayList<>();
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        dfs(nums, used, new ArrayList<>());
+        return result;
+    }
+
+    private void dfs(int[] nums, boolean[] used, List<Integer> currentList) {
+        if (currentList.size() == nums.length) {
+            result.add(new ArrayList<>(currentList));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            currentList.add(nums[i]);
+            used[i] = true;
+            dfs(nums, used, currentList);
+            currentList.remove(currentList.size() - 1);
+            used[i] = false;
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) {
+                i++;
+            }
+        }
+    }
+}
